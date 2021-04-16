ac37fb1584a3ee14652fcd4a8a749c4a6b22b48b
Addis Belete <addis.bel@gmail.com>
2021-04-16 14:02:35 +0300

    Add method to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
-
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
-  resources :require, only: [:new, :create, :index]
-  before_action :require_login, only: [:new, :create]
+  resources :posts, only: [:new, :create, :index]
 end
