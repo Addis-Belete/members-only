0a00f58e782b86f659e4f31919d846a0b23150ff
Addis Belete <addis.bel@gmail.com>
2021-04-16 22:07:55 +0300

    Add method to save the user_name to database

--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-    def user
-        @users = User.all
-    end
-end
---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class UserControllerTest < ActionDispatch::IntegrationTest
+  test "should get ind" do
+    get user_ind_url
+    assert_response :success
+  end
+end
